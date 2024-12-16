944ed997adffe1b3a8f408bfc6e28bfd88427140
John <john@iamjohnhenry.com>
2024-12-15 19:07:40 -0800

    add mocks for writing assistance API mocks

--- mock/index.mjs
+++ mock/index.mjs
@@ -1,4 +1,10 @@
 import { default as languageModel } from "./languageModel.mjs";
+import { default as writer } from "./writer.mjs";
+import { default as rewriter } from "./rewriter.mjs";
+import { default as summarizer } from "./summarizer.mjs";
 export default {
-    languageModel
+    languageModel,
+    writer,
+    rewriter,
+    summarizer
 }
